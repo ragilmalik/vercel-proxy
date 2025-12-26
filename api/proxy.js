8e08974d13e4f23661da9407b3925f291ed60316
Malik <37180346+ragilmalik@users.noreply.github.com>
2025-12-26 16:18:44 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,6 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
   let target = "https://search.brave.com";//your website url
+  
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
